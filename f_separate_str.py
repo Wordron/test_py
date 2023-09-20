11e9578a9bbf27cc9dd27a0d36b09c1f2a8d3b21
Maxime Leroy <114658480+Wordron@users.noreply.github.com>
2023-09-20 10:58:45 +0200

    Add files via upload

--- f_separate_str.py
+++ f_separate_str.py
@@ -1,10 +1,18 @@
-def separate_str(str, delim):
+def separate_str(str_to_separate, delim):
     separated_str = []
-    temp = str.partition(delim)
+
+    temp = str_to_separate.partition(delim)
     while 1:
         if len(temp[0]) != 0:
             separated_str.append(temp[0])
             if len(temp[2]) == 0:
                 return separated_str
+        else:
+            return separated_str
         temp = temp[2].partition(delim)
     return separated_str
+
+
+if __name__ == "__main__":
+    separated_str = separate_str("test_toto_hello", "_")
+    print(separated_str)
